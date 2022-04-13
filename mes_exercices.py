e4048c7eeb265147665782b65a6a33d9e0583d87
Falilou <90579427+falilou15@users.noreply.github.com>
2022-04-13 12:02:06 +0200

    Update mes_exercices.py

--- mes_exercices.py
+++ mes_exercices.py
@@ -1,7 +1,16 @@
-Exercice 08.1 :
+#Exercice 08.1 :
 
 def recherche(elt : int,tab : list) -> int:
     for i in range(len(tab)):
         if tab[i] == elt :
                 return i
     return -1
+
+#Nombre puis Double :
+
+def nombres_puis_double(valeurs: list) -> list :
+    lst = []
+    for i in range(len(valeurs)-1):
+        if valeurs[i+1] == valeurs[i] * 2:
+            lst.append((valeurs[i] , valeurs[i+1]))
+    return lst
