01c9e6ede700c212e7d7841f202bd8f00a2b4976
Falilou <90579427+falilou15@users.noreply.github.com>
2022-04-13 12:31:19 +0200

    Update mes_exercices.py

--- mes_exercices.py
+++ mes_exercices.py
@@ -1,5 +1,5 @@
 #sujets : https://pixees.fr/informatiquelycee/term/suj_bac/index.html
-#Exercice 08.1 :
+# Exercice 08.1 :
 
 def recherche(elt : int,tab : list) -> int:
     for i in range(len(tab)):
@@ -7,7 +7,7 @@ def recherche(elt : int,tab : list) -> int:
                 return i
     return -1
 
-#Nombre puis Double :
+# Nombre puis Double :
 
 def nombres_puis_double(valeurs: list) -> list :
     lst = []
@@ -15,3 +15,15 @@ def nombres_puis_double(valeurs: list) -> list :
         if valeurs[i+1] == valeurs[i] * 2:
             lst.append((valeurs[i] , valeurs[i+1]))
     return lst
+
+
+# occurrences mini
+
+def occurrences_mini(donnees : list) -> tuple:
+    m = []
+    for i in range(len(donnees)) :
+        if donnees[i] <= donnees[i+1] :
+            
+        return 
+
+print(occurrences_mini([+13, +49, +13, +5]))
